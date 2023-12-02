92e54f965566587aa7d5f5711a5f76c91689031a
roesn <rosen@qq.com>
2023-12-02 20:19:55 +0800

    just

--- rengongzhineng/5.2.自带数据集的使用.py
+++ rengongzhineng/5.2.自带数据集的使用.py
@@ -1,7 +1,9 @@
 from torchvision.datasets import MNIST
-
+from torchvision import transforms
 mnist =  MNIST(root='data',train=True,download=True)
 
 
 #print(mnist[0][0].show())
 print(mnist[0])
+ret = transforms.ToTensor()(mnist[0][0])
+print(ret.size())
